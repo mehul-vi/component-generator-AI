298864fb2548bef8c444a0f5bb5fec87b89c12d2
Mehu Kumar Sahu <mehul.www.mk@gmail.com>
2026-03-05 10:04:26 +0530

    finaal commit

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,11 @@ app.use((req, res) => {
 });
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(` Server running on port ${port}`)
-  console.log(` Environment: ${process.env.NODE_ENV || "development"}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(` Server running on port ${port}`)
+    console.log(` Environment: ${process.env.NODE_ENV || "development"}`)
+  })
+}
+
+module.exports = app;
